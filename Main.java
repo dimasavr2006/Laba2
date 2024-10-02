35b3c9fe79e31378829e3adf13378b7100bb0c9d
dimasavr <dimasavr2006@gmail.com>
2024-10-02 20:37:36 +0300

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    for (int i = 0; i < 5; i++){
-        System.out.println("i = " + i)
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + (i + 1));
         }
+    }
 }
